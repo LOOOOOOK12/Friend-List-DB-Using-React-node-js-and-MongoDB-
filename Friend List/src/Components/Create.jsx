dcdee1f1dc7ba9794ce82175079364b34f42d494
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-15 21:09:24 +0800

    update

--- Friend List/src/Components/Create.jsx
+++ Friend List/src/Components/Create.jsx
@@ -3,24 +3,36 @@ import axios from 'axios'
 import CreateStyles from '../Styles/CreateStyles'
 
 function Create() {
-    const [friends, setFriends]=useState();
-    const handleAdd = () =>{
-        axios.post('http://localhost:3001/add', {friends: friends})
+    const [name, setName]=useState();
+    const [age, setAge]=useState();
+    const [gender, setGender]=useState();
+    const [about, setAbout]=useState();
+
+    const Submit = (e) =>{
+        e.preventDefault();
+        axios.post("http://localhost:3001/createFriend", {name, age, gender, about})
         .then(result => console.log(result))
-        .catch(err => console.log(err))
+        .catch(error => console.log(error))
     }
+
 
     return (
         <CreateStyles className='create-form'>
-            <label>Enter Name:</label>
-            <input type="text" onChange={(e)=> setFriends(e.target.value)}/>
-            <label>Enter Age:</label>
-            <input type="text" />
-            <label>Enter Gender:</label>
-            <input type="text" />
-            <label>About:</label>
-            <input type="text" />
-            <button type = "button "className="Submit" onClick={handleAdd}>Add Friend</button>
+            <form onSubmit = {Submit}>
+                <label>Enter Name:</label>
+                <input type="text" onChange={(e)=> setName(e.target.value)}/>
+
+                <label>Enter Age:</label>
+                <input type="text" onChange={(e)=> setAge(e.target.value)}/>
+
+                <label>Enter Gender:</label>
+                <input type="text" onChange={(e)=> setGender(e.target.value)}/>
+
+                <label>About Your Friend:</label>
+                <input type="text" onChange={(e)=> setAbout(e.target.value)}/>
+
+                <button type = "button "className="Submit">Add Friend</button>
+            </form>
         </CreateStyles>
     )
 }
