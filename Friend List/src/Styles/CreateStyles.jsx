5cf3c07a1fb0a5dc4c798c07158b985564c33aee
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-15 21:40:10 +0800

    update

--- Friend List/src/Styles/CreateStyles.jsx
+++ Friend List/src/Styles/CreateStyles.jsx
@@ -1,11 +1,44 @@
 import styled from "styled-components"
 
 const CreateStyles = styled.div`
-form{
-    display: flex;
-    flex-direction: column;
-    gap: .5rem;
-    margin-bottom: 2rem;
+
+    form{
+        display: flex;
+        gap: .5rem;
+        margin-bottom: 2rem;
     }
+
+    .Label{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .Label label{
+        margin-bottom: .5rem;
+    }
+
+    input{
+        outline: none;
+        color: white;
+        background: transparent;
+        border: 2px white solid;
+        padding: 0.3rem;
+
+    }
+
+    .Submit{
+        color: #f8f8f8;
+        padding: 0 1.5rem;
+        background: #00a400;
+        border-radius: .4rem;
+        border: transparent;
+        transition: ease .2s;
+    }
+
+    .Submit:hover{
+        background: #007700;
+    }
+
+    
 `
 export default CreateStyles;
