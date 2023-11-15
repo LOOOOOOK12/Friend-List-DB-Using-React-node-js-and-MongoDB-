9ac17ffec1a1823d9d0a7f44ac7bb714b7fa55cd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-15 21:40:13 +0800

    update

--- Friend List/src/Styles/HomeStyles.jsx
+++ Friend List/src/Styles/HomeStyles.jsx
@@ -13,8 +13,37 @@ const HomeStyles = styled.div`
         margin-bottom: 2rem;
     }  
 
-    tr th{
-        margin: 3rem;
+    table{
+        text-align: left;
+        width: 100%;
+    }
+
+    .Edit{
+        color: white;
+        background: #018501;
+        padding: .5rem 1rem;
+        margin-right: 1rem;
+        border: 2px white none;
+        border-radius: .4rem;
+        transition: ease .2s;
+    }
+
+    .Edit:hover{
+        background: #006700;
+    }
+
+    .Delete{
+        color: white;
+        background: #ff2626;
+        padding: .5rem 1rem;
+        margin-right: 1rem;
+        border: 2px white none;
+        border-radius: .4rem;
+        transition: ease .2s;
+    }
+
+    .Delete:hover{
+        background: #d20000;
     }
 `
 export default HomeStyles;
