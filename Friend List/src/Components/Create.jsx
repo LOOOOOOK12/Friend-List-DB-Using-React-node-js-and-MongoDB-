e602496271768cb62311e01e5d01fe7d410d3155
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-16 16:04:58 +0800

    update

--- Friend List/src/Components/Create.jsx
+++ Friend List/src/Components/Create.jsx
@@ -2,41 +2,50 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import CreateStyles from '../Styles/CreateStyles'
 
-function Create() {
-    const [name, setName]=useState();
-    const [age, setAge]=useState();
-    const [gender, setGender]=useState();
-    const [about, setAbout]=useState();
+function Create(props) {
+    const [name, setName]=useState("");
+    const [age, setAge]=useState("");
+    const [gender, setGender]=useState("");
+    const [about, setAbout]=useState("");
 
-    const Submit = (e) =>{
+    const Submit = async (e) => {
         e.preventDefault();
-        axios.post("http://localhost:3001/createFriend", {name, age, gender, about})
-        .then(result => console.log(result))
-        .catch(error => console.log(error))
+    
+        axios.post("http://localhost:3001/createFriend", { name, age, gender, about })
+        .then(response => {
+            props.setFriends(prevState => [...prevState, response.data])
+            clearForm()
+        }).catch((error) => console.log(error))
     }
 
+    function clearForm() {
+        setAbout("")
+        setAge("")
+        setGender("")
+        setName("")
+    }
 
     return (
         <CreateStyles className='create-form'>
-            <form onSubmit = {Submit}>
+            <form>
                 <div className='Label'>
                     <label>Enter Name:</label>
-                    <input type="text" onChange={(e)=> setName(e.target.value)}/>
+                    <input type="text" value={name} onChange={(e)=> setName(e.target.value)}/>
                 </div>
                 <div className='Label'>
                     <label>Enter Age:</label>
-                    <input type="text" onChange={(e)=> setAge(e.target.value)}/>
+                    <input type="text" value={age} onChange={(e)=> setAge(e.target.value)}/>
                 </div>
                 <div className='Label'>
                     <label>Enter Gender:</label>
-                    <input type="text" onChange={(e)=> setGender(e.target.value)}/>
+                    <input type="text" value={gender} onChange={(e)=> setGender(e.target.value)}/>
                 </div>
                 <div className='Label'>
                     <label>About Your Friend:</label>
-                    <input type="text" onChange={(e)=> setAbout(e.target.value)}/>
+                    <input type="text" value={about} onChange={(e)=> setAbout(e.target.value)}/>
                 </div>
-                
-                <button type = "button "className="Submit">Add Friend</button>
+
+                <button type = "button "className="Submit" onClick={Submit}>Add Friend</button>
             </form>
         </CreateStyles>
     )
