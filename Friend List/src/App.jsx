370449dc5e8bba92e4a99054569a5daa4755f355
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-17 19:15:37 +0800

    update

--- Friend List/src/App.jsx
+++ Friend List/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './Home'
 import AppStyles from './Styles/AppStyles'
 import CR from './assets/pngegg.png'
+import {Modal} from "./Components/Modal"
 
 function App() {
 
