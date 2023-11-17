43f156e0ff19935aa91ef92d57c11cdaab9949fa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-17 18:21:18 +0800

    update

---
+++ Friend List/src/Styles/UpdateStyles.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+const UpdateStyles = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #aeaeff;
+
+    form{
+        color: #f7f7f7;
+        background: #7878ff;
+        padding: 3rem;
+        display: flex;
+        gap: 1rem;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .update-Button{
+        color: white;
+        background: #00a400;
+        padding: .5rem 1rem;
+        margin-right: .5rem;
+        border: 2px white none;
+        border-radius: .4rem;
+        transition: ease .2s;
+    }
+
+    .update-Button:hover{
+        background: #006700;
+    }
+
+    input{
+        outline: none;
+        color: white;
+        background: transparent;
+        border: 2px white solid;
+        padding: 0.3rem;
+
+    }
+
+    label{
+        display: flex;
+        flex-direction: column;
+    }
+`   
+export default UpdateStyles;
