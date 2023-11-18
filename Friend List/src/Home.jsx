d512bb424c6b30a8794c22fe7b02c100e4ee8f4c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-18 15:36:24 +0800

    update

--- Friend List/src/Home.jsx
+++ Friend List/src/Home.jsx
@@ -1,68 +1,91 @@
-import React, {useEffect, useState} from 'react'
-import Create from './Components/Create'
-import HomeStyles from './Styles/HomeStyles'
-import {Modal} from './Components/Modal'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import Create from './Components/Create';
+import HomeStyles from './Styles/HomeStyles';
+import { Modal } from './Components/Modal';
+import axios from 'axios';
 
 function Home() {
-    const [friend, setFriends] = useState([])
-    const [modalOpen, setModalOpen] = useState(false)
+    const [friends, setFriends] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedFriendId, setSelectedFriendId] = useState(null);
 
-    const handleButtonClick =() =>{
-        setModalOpen(false)
-    }
+    const handleButtonClick = (id) => {
+        setSelectedFriendId(id);
+        setModalOpen(true);
+    };
 
     const deleteFriend = (id) => {
-        axios.delete('http://localhost:3001/deleteFriend/' + id)
-        .then(res => {console.log(res)
-            window.location.reload()})
-        .catch(err => console.log(err))
-    }
+        axios
+        .delete('http://localhost:3001/deleteFriend/' + id)
+        .then((res) => {
+            console.log(res);
+            window.location.reload();
+        })
+        .catch((err) => console.log(err));
+    };
 
-    useEffect(()=>{
-        axios.get("http://localhost:3001")
-        .then(result => setFriends(result.data))
-        .catch(error => console.log(error))
-    },[])
+    const handleModalSubmit = (updatedFriendData) => {
+        // Update the state with the edited friend data
+        setFriends((prevFriends) =>
+            prevFriends.map((friend) =>
+                friend._id === selectedFriendId ? { ...friend, ...updatedFriendData } : friend
+            )
+            );
+            // Close the modal
+            setModalOpen(false);
+        };
+    
+
+    useEffect(() => {
+        axios.get('http://localhost:3001')
+        .then((result) => setFriends(result.data))
+        .catch((error) => console.log(error));
+    }, []);
 
     return (
         <HomeStyles>
-            <h1>Friend List!!!</h1>
-            <p>Add your friends here and never forget about them!!!</p>
-            <Create setFriends={setFriends}/>
+        <h1>Friend List!!!</h1>
+        <p>Add your friends here and never forget about them!!!</p>
+        <Create setFriends={setFriends} />
         <table>
             <thead className='Thead'>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>About Your Friend:</th>
-                </tr>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>About Your Friend:</th>
+            </tr>
             </thead>
             <tbody>
-                {
-                    friend.map((friends) =>{
-                    return<tr key ={friend._id}>
-                                <td>{friends.name}</td>
-                                <td>{friends.age}</td>
-                                <td>{friends.gender}</td>
-                                <td>{friends.about}</td>
-                                <td>
-                                    <button className='Edit' onClick={() => setModalOpen(true)}>Edit</button>
-                                        {modalOpen && 
-                                        <Modal 
-                                            onSubmit={handleButtonClick} 
-                                            onCancel={handleButtonClick}/>
-                                        }
-                                    <button className='Delete' onClick={(e) => deleteFriend(friends._id)}>Delete</button>
-                                </td>
-                        </tr>
-                    })
-                }
+            {friends.map((friend) => {
+                return (
+                <tr key={friend._id}>
+                    <td>{friend.name}</td>
+                    <td>{friend.age}</td>
+                    <td>{friend.gender}</td>
+                    <td>{friend.about}</td>
+                    <td>
+                    <button className='Edit' onClick={() => handleButtonClick(friend._id)}>
+                        Edit
+                    </button>
+                    {modalOpen && selectedFriendId && (
+                        <Modal
+                        friend={friends.find((friend) => friend._id === selectedFriendId)}
+                        onSubmit={handleModalSubmit}
+                        onCancel={() => setModalOpen(false)}
+                        />
+                    )}
+                    <button className='Delete' onClick={() => deleteFriend(friend._id)}>
+                        Delete
+                    </button>
+                    </td>
+                </tr>
+                );
+            })}
             </tbody>
         </table>
         </HomeStyles>
-    )
-}
+    );
+    }
 
-export default Home
+export default Home;
