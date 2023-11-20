92aba600669c057706c8c594399bbae705bd940b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-20 16:47:54 +0800

    update

--- Friend List/src/Components/Modal.jsx
+++ Friend List/src/Components/Modal.jsx
@@ -10,70 +10,85 @@ export const Modal = ({ friend, onSubmit, onCancel }) => {
         gender: "",
         about: "",
     });
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        // Populate the form data with the existing friend data when the modal is opened
         setFormData({
-        name: friend.name,
-        age: friend.age,
-        gender: friend.gender,
-        about: friend.about,
+            name: friend.name,
+            age: friend.age,
+            gender: friend.gender,
+            about: friend.about,
         });
     }, [friend]);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData({
-        ...formData,
-        [name]: value,
+            ...formData,
+            [name]: value,
         });
     };
 
     const handleUpdate = () => {
-        // Use axios to update the data
+        // Check for empty inputs
+        if (!formData.name || !formData.age || !formData.gender || !formData.about) {
+            setError('All fields are required.');
+            return;
+        }
+
+        // Check if age contains only numbers
+        if (!/^\d+$/.test(formData.age)) {
+            setError('Age should contain only numbers.');
+            return;
+        }
+
+        // If all checks pass, clear error and proceed with the update
+        setError('');
+
         axios
-        .put(`http://localhost:3001/updateFriend/${friend._id}`, formData)
-        .then((res) => {
-            console.log(res);
-            onSubmit(formData);
-            onCancel();
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+            .put(`http://localhost:3001/updateFriend/${friend._id}`, formData)
+            .then((res) => {
+                console.log(res);
+                onSubmit(formData);
+                onCancel();
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     };
 
     return (
         <div className='modal-Container'>
-        <div className="modal">
-            <UpdateStyles>
-            <form>
-                <h1>Update Friend</h1>
-                <div className='Label'>
-                <label>Update name:</label>
-                <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-                </div>
-                <div className='Label'>
-                <label>Update age:</label>
-                <input type="text" name="age" value={formData.age} onChange={handleInputChange} />
-                </div>
-                <div className='Label'>
-                <label>Update Gender</label>
-                <input type="text" name="gender" value={formData.gender} onChange={handleInputChange} />
-                </div>
-                <div className='Label'>
-                <label>Update About</label>
-                <input type="text" name="about" value={formData.about} onChange={handleInputChange} />
-                </div>
-                <div className='buttons'>
-                <button className='update-Button' type="button" onClick={handleUpdate}>
-                    Update
-                </button>
-                <button className='cancel-Button' onClick={() => onCancel()}>Cancel</button>
-                </div>
-            </form>
-            </UpdateStyles>
-        </div>
+            <div className="modal">
+                <UpdateStyles>
+                    <form>
+                        <h1>Update Friend</h1>
+                        {error && <div style={{ color: '#fa2222' }}>{error}</div>}
+                        <div className='Label'>
+                            <label>Update name:</label>
+                            <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+                        </div>
+                        <div className='Label'>
+                            <label>Update age:</label>
+                            <input type="text" name="age" value={formData.age} onChange={handleInputChange} />
+                        </div>
+                        <div className='Label'>
+                            <label>Update Gender</label>
+                            <input type="text" name="gender" value={formData.gender} onChange={handleInputChange} />
+                        </div>
+                        <div className='Label'>
+                            <label>Update About</label>
+                            <input type="text" name="about" value={formData.about} onChange={handleInputChange} />
+                        </div>
+                        <div className='buttons'>
+                            <button className='update-Button' type="button" onClick={handleUpdate}>
+                                Update
+                            </button>
+                            <button className='cancel-Button' onClick={() => onCancel()}>Cancel</button>
+                        </div>
+                    </form>
+                </UpdateStyles>
+            </div>
         </div>
     );
 };
