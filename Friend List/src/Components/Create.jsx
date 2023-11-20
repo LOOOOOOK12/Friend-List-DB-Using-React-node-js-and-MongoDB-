78f3dd459a2730796fcdfe3fc5d977d40823f69f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-20 16:41:16 +0800

    update

--- Friend List/src/Components/Create.jsx
+++ Friend List/src/Components/Create.jsx
@@ -1,55 +1,81 @@
-import React, { useState } from 'react'
-
-import axios from 'axios'
-import CreateStyles from '../Styles/CreateStyles'
+import React, { useState } from 'react';
+import axios from 'axios';
+import CreateStyles from '../Styles/CreateStyles';
 
 function Create(props) {
-    const [name, setName]=useState("");
-    const [age, setAge]=useState("");
-    const [gender, setGender]=useState("");
-    const [about, setAbout]=useState("");
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [gender, setGender] = useState('');
+    const [about, setAbout] = useState('');
+    const [error, setError] = useState('');
 
     const Submit = async (e) => {
         e.preventDefault();
-    
-        axios.post("http://localhost:3001/createFriend", { name, age, gender, about })
-        .then(response => {
-            props.setFriends(prevState => [...prevState, response.data])
-            clearForm()
-        }).catch((error) => console.log(error))
-    }
+
+        if (!name || !age || !gender || !about) {
+        setError('All fields are required.');
+        return;
+        }
+
+        
+        if (!/^\d+$/.test(age)) {
+            setError('Age should contain only numbers.');
+            return;
+        }
+
+        if (!/^[a-zA-Z]+$/.test(age)) {
+            setError('age should contain only letters.');
+            return;
+            }   
+        setError('');
+
+        axios
+        .post('http://localhost:3001/createFriend', { name, age, gender, about })
+        .then((response) => {
+            props.setFriends((prevState) => [...prevState, response.data]);
+            clearForm();
+        })
+        .catch((error) => console.log(error));
+    };
 
     function clearForm() {
-        setAbout("")
-        setAge("")
-        setGender("")
-        setName("")
+        setAbout('');
+        setAge('');
+        setGender('');
+        setName('');
     }
 
     return (
-        <CreateStyles className='create-form'>
-            <form>
-                <div className='Label'>
-                    <label>Enter Name:</label>
-                    <input type="text" value={name} onChange={(e)=> setName(e.target.value)}/>
+        <CreateStyles className="create-form">
+        <form>
+            <div className='Error'>
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+            </div>
+            <div className='Labels'>
+                <div className="Label">
+                <label>Enter Name:</label>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
-                <div className='Label'>
-                    <label>Enter Age:</label>
-                    <input type="text" value={age} onChange={(e)=> setAge(e.target.value)}/>
+                <div className="Label">
+                <label>Enter Age:</label>
+                <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
                 </div>
-                <div className='Label'>
-                    <label>Enter Gender:</label>
-                    <input type="text" value={gender} onChange={(e)=> setGender(e.target.value)}/>
+                <div className="Label">
+                <label>Enter Gender:</label>
+                <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
                 </div>
-                <div className='Label'>
-                    <label>About Your Friend:</label>
-                    <input type="text" value={about} onChange={(e)=> setAbout(e.target.value)}/>
+                <div className="Label">
+                <label>About Your Friend:</label>
+                <input type="text" value={about} onChange={(e) => setAbout(e.target.value)} />
                 </div>
 
-                <button type = "button "className="Submit" onClick={Submit}>Add Friend</button>
-            </form>
+                <button type="button" className="Submit" onClick={Submit}>
+                Add Friend
+                </button>
+            </div>
+        </form>
         </CreateStyles>
-    )
+    );
 }
 
-export default Create
+export default Create;
