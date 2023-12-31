6f5fdd3ee3802eacf3e6d35d5764093b5b14682c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-31 13:28:59 +0800

    update

--- Server/index.js
+++ Server/index.js
@@ -9,12 +9,14 @@ app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/Friends")
 
+//gawa friend
 app.post("/createFriend", (req,res) => {
     FriendsModel.create(req.body)
     .then(friends => res.json(friends))
     .catch(error => res.json(error))
 })
 
+//kunin yung friends
 app.get('/',(req, res) => {
     FriendsModel.find({})
     .then(friends => res.json(friends))
@@ -29,6 +31,7 @@ app.get('/getFriends/:id', (req, res) =>{
     .catch(err => res.json(err))
 })
 
+//dito iuupdate su friend sa modal
 app.put('/updateFriend/:id',(req, res) => {
     const id = req.params.id;
     FriendsModel.findByIdAndUpdate({_id: id},{
@@ -40,6 +43,7 @@ app.put('/updateFriend/:id',(req, res) => {
     .catch(err => res.json(err))
 })
 
+//delete su barkada mo
 app.delete("/deleteFriend/:id",(req, res) =>{
     const id = req.params.id;
     FriendsModel.findByIdAndDelete({_id: id})
@@ -47,7 +51,7 @@ app.delete("/deleteFriend/:id",(req, res) =>{
     .catch(error => res.json(error))
 })
 
-
+//pang open lang kung gumagana server
 app.listen(3001, () => {
     console.log("Server is Running")
 })
